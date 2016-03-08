d28387bc91a4920b99d065cbecd0584b8d1c6b91
daisydu <857635228@qq.com>
2016-03-08 21:09:07 +0100

    Update
    1. List all dishes at the first time
    2. Search the dishes
    3. Click to see the detail of selected dish
    4. In the dish detail page, list the detail information
    5. back to the search page
    
    Unfinished:
    1. confirm dish
    2. menu list

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -6,7 +6,13 @@
 dinnerPlannerApp.factory('Dinner',function ($resource) {
   
   var numberOfGuest = 2;
-
+  var menu = [];
+  var pendingmenu = [];
+  var dishType = '';
+  var filter = '';
+  var dishID;
+  this.dish = [];
+  this.dishes = [];  
 
   this.setNumberOfGuests = function(num) {
     numberOfGuest = num;
@@ -16,7 +22,60 @@ dinnerPlannerApp.factory('Dinner',function ($resource) {
     return numberOfGuest;
   }
 
+  this.setDishID = function(id){
+        dishID = id;
+  }
 
+  this.getDishID = function(){
+    //console.log("get id"+dishID);
+    return dishID;
+  }
+
+  this.setFilter = function(flt){
+        filter = flt;
+        //console.log("i am " + filter);
+  }
+
+  this.getFilter = function(){
+        return filter;
+  }
+
+  this.getFullMenu = function() {
+    //TODO Lab 2
+    var dishesOnMenu = [];
+    for (var i = 0; i < menu.length; i++) {
+      dishesOnMenu.push(this.getLocalDish(menu[i]));
+
+    };
+    // console.log(dishesOnMenu);
+        return dishesOnMenu;
+  }
+
+  var th = this;
+  // var apiKey = "18f3cT02U9f6yRl3OKDpP8NA537kxYKu";
+  // var apiKey = "XKEdN82lQn8x6Y5jm3K1ZX8L895WUoXN";
+  // var apiKey = "3stL5NVP4s6ZkmK5gt4dci8a4zOQRpD4";
+  // var apiKey = "8vtk7KykflO5IzB96kb0mpot0sU40096";
+  // var apiKey = "1hg3g4Dkwr6pSt22n00EfS01rz568IR6";
+   var apiKey = "r02x0R09O76JMCMc4nuM0PJXawUHpBUL";
+  // var apiKey = "H9n1zb6es492fj87OxDtZM9s5sb29rW3";
+
+  this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:5,api_key: apiKey});
+  this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key: apiKey}); 
+  dishes = this.DishSearch.get({});
+  //console.log(dishes.Results);
+  // dish = this.Dish.get({id:12345});
+  //console.log(dish);
+  this.Dish.get({id:12345},function(data){
+    th.dish=data;
+    //console.log(th.dish.Title);
+    });
+
+    this.DishSearch.get({title_kw:"main"},function(data){
+    th.dishes = data.Results;
+    //console.log(th.dishes);
+  });
+  
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
   // you will need to modify the model (getDish and getAllDishes) 
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -4,5 +4,23 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
+  Dinner.DishSearch.get(function(data){
+     $scope.dishes=data.Results;
+     //console.log($scope.dishes);
+     $scope.status = "Showing " + data.Results.length + " results";
+   },function(data){
+     $scope.status = "There was an error";
+   });
 
+  $scope.search = function(query) {
+    $scope.status = "Searching...";
+   Dinner.DishSearch.get({title_kw:query},function(data){
+     $scope.dishes=data.Results;
+     //console.log($scope.dishes);
+     $scope.status = "Showing " + data.Results.length + " results";
+   },function(data){
+     $scope.status = "There was an error";
+   });
+ }
+  
 });
